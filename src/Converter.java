261358e6731b0c92b494516289f77f2539b8bbaf
SLiYDD <mudritsyn@yandex.ru>
2022-11-28 15:28:43 +0300

    Третий Коммит

--- src/Converter.java
+++ src/Converter.java
@@ -1,3 +1,12 @@
 public class Converter {
+    double sizeStep = 0.75;
+    int caloriesPerStep = 50;
+    public double stepToKM(int sumToMonth){
+        double distance = sumToMonth * sizeStep;
+        return distance/1000;
+    }
 
+    public void stepToCalories(){
+
+    }
 }
